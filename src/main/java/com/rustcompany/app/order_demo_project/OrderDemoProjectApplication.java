a1e5ac2d2bb217851eb9f391ae24975c4a707007
Msp88 <aleast.88@gmail.com>
2022-09-11 02:16:31 +0300

    Initial commit

---
+++ src/main/java/com/rustcompany/app/order_demo_project/OrderDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rustcompany.app.order_demo_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderDemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderDemoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rustcompany/app/order_demo_project/OrderDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rustcompany.app.order_demo_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderDemoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
